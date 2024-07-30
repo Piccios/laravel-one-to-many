2deb9b5b169f18b6ca76c314e87cea8c535f833b
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-30 15:25:25 +0200

    created a reference to types and projects

---
+++ database/migrations/2024_07_30_131741_add_type_id_to_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id');
+            //vincolo di chiave esterna sulla mia colonna x che fa rifierimento alla colonna y della tabella z
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as faker;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $typesName = [
+            "Laravel",
+            "Back-end",
+            "Front-end",
+            "PHP",
+            "JavaScript",
+            "React",
+            "Vue",
+            "HTML",
+        ];
+        foreach ($typesName as $typeName) {
+            $type = new Type();
+            $type-> name = $typeName;
+            $type-> color = $faker->unique()->safeHexColor();
+            $type->save();
+
+        }
     }
 }
